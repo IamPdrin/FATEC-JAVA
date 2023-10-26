8b185865e76297dae5ea3d13c3264668cf627492
Pedro Wolski <pedrobwolski@gmail.com>
2023-10-26 03:10:02 -0300

    6POO

--- Atv6/src/Carro.java
+++ Atv6/src/Carro.java
@@ -2,12 +2,13 @@ public class Carro {
     //Atributos:
     public String marca;
     public String modelo;
-    public String motor;
+    public String motor = null;
     public int ano;
     public double tanque;
+    public double tanqueAtual = 0;
     public double velocidade = 0;
     public boolean status = false;
-    public boolean statusMotor = false;
+
 
     //Construtor:
     public Carro(String marca, String modelo, String motor, int ano, double tanque){
@@ -20,46 +21,86 @@ public class Carro {
 
 
     //Funções:
-    public void girarChave () {
-            if(status == false){
-                status = true;
-                System.out.println("Ligou\n");
-            }
-            else {
-                status = false;
-                System.out.println("Desligou\n");
-            }
+    public void verificarMotor(){
+        if(this.motor != null){
+            System.out.println("O carro possui motor!\n");
+            System.out.println("Motor: " + this.motor);
+        }
+        else {
+            System.out.println("O carro nao possui motor!\n");
+        }
     }
 
-    public int abastecer (double gasolina){
-        double litros = 0;
-        litros = this.tanque;
+    public void girarChave () {
+        if(status == false && this.motor != null){
+            status = true;
+            System.out.println("Ligou");
+        }
+        else if (this.motor == null){
+            System.out.println("O carro nao possui motor, nao e possivel ligar!");
+        }
+        else{
+            status = false;
+            System.out.println("Desligou");
+        }
+    }
+
+    public void abastecer (double gasolina){
+        double litros = this.tanque;
         double res = litros * gasolina;
         
         System.out.println("A capacidade maxima do tanque do " + this.marca + " " + this.modelo + " é de " + this.tanque + " litros.");        
-        System.out.println("O valor para encher o tanque é de R$" + res + "\n");
-        
-        return 0;
+        System.out.println("O valor para encher o tanque e de R$" + res + "\n");
+        System.out.println("Abastecendo..");
+
+        if (this.tanqueAtual <= 80 && this.velocidade == 0){
+            this.tanqueAtual = 80;
+        }
+        else if (this.velocidade > 0){
+            System.out.println("Pare o carro para abastecer!");
+        }
+        else {
+            System.out.println("O tanque ja esta cheio.");
+        }
     }
         
     public double acelerar(){
-        if(statusMotor == true){
+        if(this.status == true && this.tanqueAtual > 0 && this.velocidade >= 0 && this.velocidade < 180){
+            System.out.println("Acelerando..");
             this.velocidade+=20;
+            this.tanqueAtual-=5;
+        }
+        else if (this.status == false){
+            System.out.println("Para acelerar, ligue o carro primeiro!");
+        }
+        else if (this.velocidade >= 180.0){
+            System.out.println("O carro atingiu sua velocidade maxima");
+            this.status = false;
+        }
+        else {
+            System.out.println("O carro esta sem gasolina");
         }
         return this.velocidade;
     }
         
     public double frear(){
-        this.velocidade-=20;
+        if(this.velocidade > 0){
+            this.velocidade-=30;
+        }
+        if (this.velocidade < 0){
+            this.velocidade = 0;
+        }
+        else{
+            System.out.println("Nao e possivel frear se o carro estiver parado!");
+        } 
         return this.velocidade;
     }
         
     public double getVelocidade(){
         return this.velocidade;
     }
-        
-    public void setVelocidade(double vel){
-        this.velocidade += vel;
+
+    public double getTanqueAtual(){
+        return this.tanqueAtual;
     }
-    
 }
--- Atv6/src/Main.java
+++ Atv6/src/Main.java
@@ -1,22 +1,79 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        Carro car1 = new Carro("Porsche", "Panamera", "V8 biturbo 620cv", 2023, 80.00);  
+        Carro car1 = new Carro("Porsche", "Panamera", "V8 biturbo 620cv", 2023, 80.00);
+        Console cons = new Console();
+        
+        Scanner ler = new Scanner(System.in);        
+        int escolha = 0;
 
-        System.out.println("Função de girar chave: \n");
-        //Ligando carro:
-        car1.girarChave();
-        //Desligando carro:
-        car1.girarChave();
-        //Ligando novamente:
-        car1.girarChave();
+        do {
+            System.out.println("--------------------------------");
+            System.out.println("      POO: Encapsulamento       ");
+            System.out.println("         Class: Carro           ");
+            System.out.println("--------------------------------");
 
-        System.out.println("Função de acelerar: \n");
-        //Acelerando carro:
-        car1.acelerar();
+            System.out.println("--------------------------------");
+            System.out.print("Status: ");
+            if(car1.status == true){
+                System.out.println("O carro esta ligado");
+                System.out.println("Gasolina: " + car1.getTanqueAtual());
+                System.out.println("Velocidade: " + car1.getVelocidade());
+            }
+            else{
+                System.out.println("O carro esta desligado");
+            }
+            System.out.println("--------------------------------");
 
-        System.out.println("Função de calcular o valor para encher o tanque: \n");
-        //Calculando o valor para encher o tanque
-        car1.abastecer(5.75);
+            System.out.println("\nEscolha uma das opções abaixo: ");
+            System.out.println("  1-Verficar Motor\n  2-Ligar/Desligar Carro\n  3-Acelerar Carro\n  4-Frear Carro\n  5-Encher o tanque\n  6-Sair do carro\n");
+            escolha = ler.nextInt();
+
+            switch(escolha){
+                case 1:
+                    //Verificando se o atributo motor foi preenchido:
+                    car1.verificarMotor();
+                    break;
+
+                case 2:
+                    //Ligando/Desligando carro:
+                    car1.girarChave();
+                    break;
+
+                case 3:
+                    //Acelerando carro:
+                    car1.acelerar();
+                    break;
+
+                case 4:
+                    //Freando carro:
+                    car1.frear();
+                    break;
+
+                case 5:
+                    //Calculando o valor para encher o tanque
+                    car1.abastecer(5.75);
+                    break;
+
+                case 6:
+                    System.out.println("Saindo do carro..\n");
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida.\n");
+            }
+
+            System.out.print("\nContinue(Enter)..");
+            ler.nextLine();
+            ler.nextLine();
+            cons.limparConsole();
+
+        } while (escolha != 6);
+
+        ler.close();
+
     }
 }
