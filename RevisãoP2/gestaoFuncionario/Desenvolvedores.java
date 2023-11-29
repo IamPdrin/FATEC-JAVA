9f1613cb01be29fa093d0322bf4bd48df53b32ff
Pedro Wolski <pedrobwolski@gmail.com>
2023-11-29 20:38:19 -0300

    del

--- RevisãoP2/gestaoFuncionario/Desenvolvedores.java
+++
@@ -1,9 +0,0 @@
-package gestaoFuncionario;
-
-public class Desenvolvedores extends Funcionario {
-
-	Desenvolvedores(String nome, int idade, String funcao) {
-		super(nome, idade, funcao);
-	}
-
-}
--- RevisãoP2/gestaoFuncionario/Gerente.java
+++
@@ -1,9 +0,0 @@
-package gestaoFuncionario;
-
-public class Gerente extends Funcionario{
-
-	Gerente(String nome, int idade, String funcao) {
-		super(nome, idade, funcao);
-	}
-	
-}
